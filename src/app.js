b0192154967b4f69e1c1ae2bf43745f7a5e8edc0
Rodrigo Medeiros <dev.rodrigomedeiros@gmail.com>
2025-06-02 19:35:55 -0300

    criando routes

--- src/app.js
+++ src/app.js
@@ -1,7 +1,22 @@
 import express from "express";
+import sequelize from "./config/db.js";
+import router from "./routes/router.js";
 
 const app = express();
 
 app.use(express.json());
+
+//rotas
+app.use("/api", router);
+
+//Sincronizar as tabelas do banco
+sequelize
+  .sync()
+  .then(() => {
+    console.log("Banco de Dados sincronizado com sucesso.");
+  })
+  .catch((error) => {
+    console.error("Erro ao sincronizar o banco de dados: ", error);
+  });
 
 export default app;
