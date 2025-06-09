308b3bb2f960f1c4b5d2249fa8e45523a8adfaef
Rodrigo Medeiros <dev.rodrigomedeiros@gmail.com>
2025-06-09 20:01:29 -0300

    model endereco finalizado

--- src/models/enderecoModel.js
+++ src/models/enderecoModel.js
@@ -1,36 +1,51 @@
 import { DataTypes } from "sequelize";
 import sequelize from "../config/db.js";
+import Cliente from "./clienteModel.js";
 
-const Endereco = sequelize.define("Endereco", {
-  id: {
-    type: DataTypes.INTEGER,
-    primaryKey: true,
-    autoIncrement: true,
-  },
-  cliente_id: {
-    type: DataTypes.INTEGER,
-    allowNull: false,
-    references: {
-      model: "clientes",
-      key: "id",
+const Endereco = sequelize.define(
+  "Endereco",
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
     },
-    onDelete: "CASCADE",
-    onUpdate: "CASCADE",
+    cliente_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        model: "clientes",
+        key: "id",
+      },
+      onDelete: "CASCADE",
+      onUpdate: "CASCADE",
+    },
+    rua: {
+      type: DataTypes.STRING(100),
+      allowNull: false,
+    },
+    cidade: {
+      type: DataTypes.STRING(50),
+      allowNull: false,
+    },
+    estado: {
+      type: DataTypes.STRING(2),
+      allowNull: false,
+    },
+    cep: {
+      type: DataTypes.STRING(9),
+      allowNull: false,
+    },
   },
-  rua: {
-    type: DataTypes.STRING(100),
-    allowNull: false,
-  },
-  cidade: {
-    type: DataTypes.STRING(50),
-    allowNull: false,
-  },
-  estado: {
-    type: DataTypes.STRING(2),
-    allowNull: false,
-  },
-  cep: {
-    type: DataTypes.STRING(9),
-    allowNull: false,
-  },
-});
+  {
+    tableName: "enderecos",
+    timestamps: true,
+    createdAt: "created_at",
+    updatedAt: "updated_ad",
+  }
+);
+
+//Associação com Cliente
+Endereco.belongsTo(Cliente, { foreignKey: "cliente_id", as: "cliente" });
+
+export default Endereco;
