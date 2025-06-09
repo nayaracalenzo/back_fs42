48049b7793ead5e3019813fb730290ac110cc85c
Rodrigo Medeiros <dev.rodrigomedeiros@gmail.com>
2025-06-09 19:48:49 -0300

    cliente finalizado

--- src/repositories/clienteRepository.js
+++ src/repositories/clienteRepository.js
@@ -7,3 +7,30 @@ export async function findAllClientes() {
 export async function findClienteById(id) {
   return await Cliente.findByPk(id);
 }
+
+export async function createCliente({ nome, email, telefone }) {
+  return await Cliente.create({ nome, email, telefone });
+}
+
+export async function updateCliente(id, { nome, email, telefone }) {
+  const cliente = await Cliente.findByPk(id);
+
+  if (!cliente) {
+    return null;
+  }
+
+  await Cliente.update({ nome, email, telefone });
+
+  return cliente;
+}
+
+export async function deleteCliente(id) {
+  const cliente = await Cliente.findByPk(id);
+
+  if (!cliente) {
+    return null;
+  }
+
+  await cliente.destroy();
+  return cliente;
+}
