e71c8ea3c429aa91db8236a4336388a58b7e7338
Rodrigo Medeiros <dev.rodrigomedeiros@gmail.com>
2025-06-23 21:07:45 -0300

    atulizando backend

--- src/app.js
+++ src/app.js
@@ -1,10 +1,15 @@
 import express from "express";
 import sequelize from "./config/db.js";
 import router from "./routes/router.js";
+import cors from "cors";
+import corsConfig from "./config/corsConfig.js";
+import cookieParser from "cookie-parser";
 
 const app = express();
 
 app.use(express.json());
+app.use(cors(corsConfig));
+app.use(cookieParser());
 
 //rotas
 app.use("/api", router);
---
+++ src/config/corsConfig.js
@@ -0,0 +1,4 @@
+export default {
+  origin: "*", //em produção, troque o * por ["https://meudominio.com"]
+  credentials: true,
+};
